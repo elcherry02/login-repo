8873ca5605f6e1800c0fd8c7f90bd6986c869a3c
El Cherry Mae Masibay <111100461+elcherry02@users.noreply.github.com>
2023-03-29 10:39:02 +0800

    created login page

---
+++ login.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Log In Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<style>
+    body {
+    font-family: 'Manrope', sans-serif;
+    height: 100vh;
+    background-image: url("img/Home.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;   
+    }
+
+    p1 {
+    font-family: 'Manrope', sans-serif;
+    font-size: 25px;
+    color: green;
+    text-align: center;
+    }
+
+    a:link {
+    color: green;
+    background-color: transparent;
+    text-decoration: none;
+    }
+
+    a:hover {
+    color: blue;
+    background-color: transparent;
+    text-decoration: underline;
+    }
+
+    img {
+    height: 250px;
+    width: 250px;
+    border-radius: 50%;
+    }
+
+    .bg-glass {
+      background-color: hsla(0, 0%, 100%, 0.9) !important;
+      backdrop-filter: saturate(200%) blur(25px);
+    }
+
+    .custom-checkbox .custom-control-input:checked ~ .custom-control-label::before {
+  background-color: green!important;
+    }
+
+</style>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        
+        <a class="navbar-brand" href="#"></a>
+        <p1><b>Visvis Travel Inc.</b></p1>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-4 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Guest</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Invoice</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Tours</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Reports</a>
+                </li>
+            </ul>
+            <button class="btn btn-outline-success" type="button">Sign up</button>
+        </div>
+    </div>
+    </nav>
+
+    <!-- Section: Design Block -->
+<section class="login">
+
+  <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+    <div class="row gx-lg-5 align-items-center mb-5">
+      <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
+      <img src="img/visvis-logo.jpg" alt="Logo" class="d-inline-block align-text-top">
+        <h1 class="my-5 display-5 fw-bold ls-tight" style="color: white">
+          The best offer <br />
+          <span style="color: #245953">for your business</span>
+        </h1>
+        
+      </div>
+
+      <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
+        <div id="radius-shape-1" class="position-absolute rounded-circle shadow-5-strong"></div>
+        <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
+
+        <div class="card bg-glass">
+          <div class="card-body px-7 py-5 px-md-4">
+            <form>
+                <!-- 2 column grid layout with text inputs for the first and last names -->
+                <div class="row">
+                
+                <!-- Email input -->
+                <div class="col-md-8 mb-2 mx-auto">
+                    <p1>Welcome!</p1><br>
+                    <p style="color:#245953">Please log in your correct credentials.</p>
+                <div class="form-outline">
+                    <input type="email" id="form3Example3" class="form-control" placeholder="Email Address" />
+                    <label class="form-label" for="form3Example3"></label>
+                </div>
+
+                <!-- Password input -->
+                <div class="form-outline">
+                    <input type="password" id="form3Example4" class="form-control" placeholder="Password" />
+                    <label class="form-label" for="form3Example4"></label>
+                </div>
+
+              <div class="d-flex justify-content-around align-items-center mb-4">
+                <!-- Checkbox -->
+                <div class="myTest custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="customCheck1" />
+                <label class="custom-control-label" for="customCheck1">Remember Me</label>
+                </div>
+                <a href="#!">Forgot password?</a>
+            </div>
+
+              <!-- Submit button -->
+              <div class="d-flex justify-content-around align-items-center mb-4">
+              <button type="submit" class="btn btn-success btn-block">
+                Log In
+              </button>
+              </div>
+
+              <!-- Register buttons -->
+              <div class="text-center">
+                <p>or sign up with:</p>
+                <button type="button" class="btn btn-link btn-floating mx-1">
+                <i class="fa-brands fa-facebook"></i>
+                </button>
+
+                <button type="button" class="btn btn-link btn-floating mx-1">
+                  <i class="fab fa-google"></i>
+                </button>
+
+                <button type="button" class="btn btn-link btn-floating mx-1">
+                  <i class="fab fa-twitter"></i>
+                </button>
+
+                <button type="button" class="btn btn-link btn-floating mx-1">
+                  <i class="fab fa-github"></i>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Section: Design Block -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/5f976d62f4.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
